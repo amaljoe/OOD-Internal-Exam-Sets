ddec9736c948778a8f33ff63838dbaf9aad4f9ad
amaljoe <amaljoe88@gmail.com>
2021-02-21 19:50:26 +0530

    cube spher and parameter initial

---
+++ Cube Sphere - Classes and Objects/CubeSphere.java
@@ -0,0 +1,12 @@
+// Create a class Cube and a class Sphere with private data members side and
+// radius respectively. Both should also contain a private variable volume
+// indicating the volume of Cube and Sphere.
+// Use suitable constructors to initialize the instance variables. Write suitable
+// functions required to return (a) positive if the volume of Cube object is less
+// than the Sphere object
+// (b) 0 if their volumes are the same
+// (c ) Negative otherwise.
+
+public class CubeSphere {
+    
+}
---
+++ Parameter - pass by value & refernce/Parameter.java
@@ -0,0 +1,11 @@
+// ii) Create a class named Parameter with two alternate functions specified below,
+// of which each simply triples the variable count defined in main()
+// (a) Function tripleByValue() passes a copy of count by value, triple the copy
+// and returns the new value
+// (b) Function tripleByReference() passes count by reference via a reference
+// parameter triple the original value of count.
+// Write a program to compare the two functions
+
+public class Parameter {
+    
+}
